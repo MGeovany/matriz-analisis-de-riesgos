176c48f080773c0fd2910bc53bea25c710d1c7e6
mgeovany <marlongeo1999@gmail.com>
2022-08-10 16:49:51 -0600

    agregado react router dom y las rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import './App.css'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Dashboard } from './pages/Dashboard'
+import { Sidebar } from './components/Sidebar'
+import { Riesgos } from './pages/riesgos/Riesgos'
+
 function App() {
-  return <div className='App'>Matriz de Riesgos 🌟</div>
+  return (
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/riesgos' element={<Riesgos />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/riesgos/Riesgos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RiesgosStyles.css'
+export const Riesgos = () => {
+  return (
+    <>
+      <div className='riesgos'>
+        <p>Riesgos</p>
+        <p className='content'>Riesgos contenido</p>
+      </div>
+    </>
+  )
+}
