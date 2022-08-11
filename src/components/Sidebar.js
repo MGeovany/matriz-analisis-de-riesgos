679868047ddcadb2f5c3aed735af1404af49fa2b
mgeovany <marlongeo1999@gmail.com>
2022-08-10 18:05:13 -0600

    se establecieron las routas y componente de sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,142 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createStyles, Navbar, Group } from '@mantine/core'
+import {
+  IconLayoutDashboard,
+  IconAlertTriangle,
+  IconBooks,
+  IconTimeline,
+  IconLogout,
+  IconCalendarEvent
+} from '@tabler/icons'
+import { Link } from 'react-router-dom'
+import './SidebarStyles.css'
+
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon')
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 2.5
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
+      }`
+    },
+
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: '#ffffff',
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black
+        }
+      }
+    },
+
+    linkIcon: {
+      ref: icon,
+      color:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[2]
+          : theme.colors.gray[6],
+      marginRight: theme.spacing.sm
+    },
+
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({
+          variant: 'light',
+          color: theme.primaryColor
+        }).background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+          .color,
+        [`& .${icon}`]: {
+          color: theme.fn.variant({
+            variant: 'light',
+            color: theme.primaryColor
+          }).color
+        }
+      }
+    }
+  }
+})
+
+const data = [
+  { label: 'Dashboard', icon: IconLayoutDashboard, route: '/' },
+  { label: 'Riesgos', icon: IconAlertTriangle, route: '/riesgos' },
+  {
+    label: 'Planes de contingencia',
+    icon: IconTimeline,
+    route: '/planes'
+  },
+  { label: 'Procesos', icon: IconBooks, route: '/procesos' },
+  { label: 'Eventos', icon: IconCalendarEvent, route: '/eventos' }
+]
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>
+  const { classes, cx } = useStyles()
+  const [active, setActive] = useState('Dashboard')
+
+  const links = data.map((item) => (
+    <Link
+      key={item.label}
+      className={cx(classes.link, {
+        [classes.linkActive]: active === item.label
+      })}
+      to={item.route}
+      onClick={(event) => {
+        event.preventDefault()
+        setActive(item.label)
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </Link>
+  ))
+
+  return (
+    <div className='sidebar'>
+      <Navbar height={700} width={{ sm: 300 }} p='md'>
+        <Navbar.Section grow>
+          <Group className={classes.header} position='apart'>
+            <span sx={{ fontWeight: 700 }}>
+              Matriz de Analisis de Riesgos 👷‍♂️
+            </span>
+          </Group>
+          {links}
+        </Navbar.Section>
+
+        <Navbar.Section className={classes.footer}>
+          <a
+            href='#'
+            className={classes.link}
+            onClick={(event) => event.preventDefault()}
+          >
+            <IconLogout className={classes.linkIcon} stroke={1.5} />
+            <span>Cerrar Sesion</span>
+          </a>
+        </Navbar.Section>
+      </Navbar>
+    </div>
+  )
 }
