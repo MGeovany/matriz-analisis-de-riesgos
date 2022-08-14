895b67919fd0e41dea80e057b04dc96554c03e09
Geovany <marlongeo1999@gmail.com>
2022-08-14 17:05:05 -0600

    iconos agregados de tabler icons

--- src/pages/riesgos/TablaRiesgo.js
+++ src/pages/riesgos/TablaRiesgo.js
@@ -2,54 +2,159 @@ import React from 'react'
 import { Table } from '@mantine/core'
 import './RiesgosStyles.css'
 import './TablaRiesgo.css'
+import { IconEye, IconTrash } from '@tabler/icons'
 
-const ojo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/OOjs_UI_icon_eye-progressive.svg/1024px-OOjs_UI_icon_eye-progressive.svg.png'
-const borrar = 'https://www.shareicon.net/data/512x512/2017/06/21/887258_delete_512x512.png'
+const ojo =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/OOjs_UI_icon_eye-progressive.svg/1024px-OOjs_UI_icon_eye-progressive.svg.png'
+const borrar =
+  'https://www.shareicon.net/data/512x512/2017/06/21/887258_delete_512x512.png'
 
 const elements = [
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> },
-  { name: 'Incendio', impact: 'Alto', prob: 'Alto', risk: 'Alto', view: <img className='ojo' src={ojo}/>, delete: <img className='borrar' src={borrar}/> }
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  },
+  {
+    name: 'Incendio',
+    impact: 'Alto',
+    prob: 'Alto',
+    risk: 'Alto',
+    view: <img className='ojo' src={ojo} />,
+    delete: <img className='borrar' src={borrar} />
+  }
 ]
 
 export function TablaRiesgos() {
-  const rows = elements.map((element) => (
+  const rows = elements.map((element, index) => (
     <tr key={element.name}>
       <td>{element.name}</td>
       <td>{element.impact}</td>
       <td>{element.prob}</td>
       <td>{element.risk}</td>
-      <td>{element.view}</td>
-      <td>{element.delete}</td>
+      <td>
+        <IconEye cursor='pointer' color='#228be6' />
+      </td>
+      <td>
+        <IconTrash cursor='pointer' color='#f05761' />
+      </td>
     </tr>
   ))
 
   return (
     <div className='contenedorTabla'>
-      <Table>
-      <thead>
-        <tr>
-          <th>Nombre de Riesgo</th>
-          <th>Impacto</th>
-          <th>Posibilidad</th>
-          <th>Riesgo</th>
-          <th>Ver</th>
-          <th>Eliminar</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+      <Table highlightOnHover>
+        <thead>
+          <tr>
+            <th>Nombre de Riesgo</th>
+            <th>Impacto</th>
+            <th>Posibilidad</th>
+            <th>Riesgo</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
     </div>
   )
 }
