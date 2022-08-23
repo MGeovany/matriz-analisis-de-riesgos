2efda12a7640fefc16737b4a9d64117fbe47379e
Geovany <marlongeo1999@gmail.com>
2022-08-23 10:35:21 -0600

    Update Form.js

--- src/pages/riesgos/form.js
+++ src/pages/riesgos/form.js
@@ -1,5 +1,18 @@
+/* eslint-disable multiline-ternary */
 import React from 'react'
-import { Modal, TextInput, Group, Button, Grid, Select, Checkbox, Stack, Textarea, Badge, Text } from '@mantine/core'
+import {
+  Modal,
+  TextInput,
+  Group,
+  Button,
+  Grid,
+  Select,
+  Checkbox,
+  Stack,
+  Textarea,
+  Badge,
+  Text
+} from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { Opciones } from '../../API'
 
@@ -33,17 +46,34 @@ export default function Form({
     }
   })
   React.useState(() => {
-    Opciones().Impacto().then(data => setListaImpacto(data)).catch(error => console.error(error))
-    Opciones().Posibilidad().then(data => setListaPosibilidad(data)).catch(error => console.error(error))
-    Opciones().NivelRiesgo().then(data => setListaNivelRiesgo(data)).catch(error => console.error(error))
+    Opciones()
+      .Impacto()
+      .then((data) => setListaImpacto(data))
+      .catch((error) => console.error(error))
+    Opciones()
+      .Posibilidad()
+      .then((data) => setListaPosibilidad(data))
+      .catch((error) => console.error(error))
+    Opciones()
+      .NivelRiesgo()
+      .then((data) => setListaNivelRiesgo(data))
+      .catch((error) => console.error(error))
   }, [])
   function submitForm() {
     handler({
       ...form.values,
-      PuntajeImpacto: ListaImpacto.find(item => item.value === form.values.IdImpacto).puntaje,
-      NivelImpacto: ListaImpacto.find(item => item.value === form.values.IdImpacto).label,
-      PuntajePosibilidad: ListaPosibilidad.find(item => item.value === form.values.IdPosibilidad).puntaje,
-      NivelPosibilidad: ListaPosibilidad.find(item => item.value === form.values.IdPosibilidad).label,
+      PuntajeImpacto: ListaImpacto.find(
+        (item) => item.value === form.values.IdImpacto
+      ).puntaje,
+      NivelImpacto: ListaImpacto.find(
+        (item) => item.value === form.values.IdImpacto
+      ).label,
+      PuntajePosibilidad: ListaPosibilidad.find(
+        (item) => item.value === form.values.IdPosibilidad
+      ).puntaje,
+      NivelPosibilidad: ListaPosibilidad.find(
+        (item) => item.value === form.values.IdPosibilidad
+      ).label,
       NivelRiesgoDescripcion: getNivelRiesgo(form.values.NivelRiesgo)
     })
     setOpen(false)
@@ -51,86 +81,133 @@ export default function Form({
   return (
     <>
       <div onClick={() => setOpen(true)}>{Trigger}</div>
-      <Modal size="xl"
-      centered
+      <Modal
+        size='xl'
+        centered
         opened={open}
         onClose={() => setOpen(false)}
         title='Nuevo Riesgo'
-        gutter="xl">
-          <form>
-          <Grid grow gutter="xl">
+        gutter='xl'
+      >
+        <form>
+          <Grid grow gutter='xl'>
             <Grid.Col md={4} xs={12}>
-              <Stack >
-              <TextInput
-                label='Nombre'
-                placeholder='Nombre'
-                {...form.getInputProps('Nombre')}
-              />
+              <Stack>
+                <TextInput
+                  label='Nombre'
+                  placeholder='Nombre'
+                  {...form.getInputProps('Nombre')}
+                />
 
                 <Select
-                  label="Nivel de impacto"
+                  label='Nivel de impacto'
                   id='IdImpacto'
-                  placeholder="Escoja una opción"
+                  placeholder='Escoja una opción'
                   itemComponent={ItemList}
                   {...form.getInputProps('IdImpacto')}
                   data={ListaImpacto}
                 />
 
                 <Select
-                  label="Nivel de Posibilidad"
+                  label='Nivel de Posibilidad'
                   id='IdPosibilidad'
-                  placeholder="Escoja una opción"
+                  placeholder='Escoja una opción'
                   itemComponent={ItemList}
                   {...form.getInputProps('IdPosibilidad')}
                   data={ListaPosibilidad}
                 />
                 <span>
-                  <Text size="lg">Nivel de Riesgo</Text>
-                    <NivelRiesgoItem
-                      puntaje={ getPuntaje(form.values.IdImpacto, form.values.IdPosibilidad) }
-                      texto={ getNivelRiesgo(getPuntaje(form.values.IdImpacto, form.values.IdPosibilidad)) }
-                      sx={{ height: '3rem', fontSize: '1.3rem' }}
-                    />
+                  <Text size='lg'>Nivel de Riesgo</Text>
+                  <NivelRiesgoItem
+                    puntaje={getPuntaje(
+                      form.values.IdImpacto,
+                      form.values.IdPosibilidad
+                    )}
+                    texto={getNivelRiesgo(
+                      getPuntaje(
+                        form.values.IdImpacto,
+                        form.values.IdPosibilidad
+                      )
+                    )}
+                    sx={{ height: '3rem', fontSize: '1.3rem' }}
+                  />
                 </span>
               </Stack>
             </Grid.Col>
 
             <Grid.Col lg={6} xs={12}>
-            <Stack>
-              <Text>Intereses Afectados</Text>
-              <Group >
-                <Checkbox label="Afecta Costo" radius="md" {...form.getInputProps('AfectaCosto', { type: 'checkbox' }) }/>
-                {(form.getInputProps('AfectaCosto').value)
-                  ? (<TextInput placeholder='$.' size='xs' {...form.getInputProps('ValorCosto') }/>)
-                  : null}
-              </Group>
-              <Group >
-                <Checkbox label="Afecta Tiempo" radius="md" {...form.getInputProps('AfectaTiempo', { type: 'checkbox' }) }/>
-                {form.getInputProps('AfectaTiempo').value
-                  ? (<TextInput size='xs' placeholder='Horas' {...form.getInputProps('ValorTiempo') }/>)
-                  : null}
-              </Group>
+              <Stack>
+                <Text>Intereses Afectados</Text>
+                <Group>
+                  <Checkbox
+                    label='Afecta Costo'
+                    radius='md'
+                    {...form.getInputProps('AfectaCosto', { type: 'checkbox' })}
+                  />
+                  {form.getInputProps('AfectaCosto').value ? (
+                    <TextInput
+                      placeholder='$.'
+                      size='xs'
+                      {...form.getInputProps('ValorCosto')}
+                    />
+                  ) : null}
+                </Group>
+                <Group>
+                  <Checkbox
+                    label='Afecta Tiempo'
+                    radius='md'
+                    {...form.getInputProps('AfectaTiempo', {
+                      type: 'checkbox'
+                    })}
+                  />
+                  {form.getInputProps('AfectaTiempo').value ? (
+                    <TextInput
+                      size='xs'
+                      placeholder='Horas'
+                      {...form.getInputProps('ValorTiempo')}
+                    />
+                  ) : null}
+                </Group>
 
-              <Group >
-                 <Checkbox label="Afecta Calidad" radius="md" { ...form.getInputProps('AfectaCalidad', { type: 'checkbox' }) }/>
-              </Group>
-                <Textarea label="Descripción" placeholder="Descripción" minRows={6} maxRows={6} { ...form.getInputProps('Descripcion') }/>
-                </Stack>
+                <Group>
+                  <Checkbox
+                    label='Afecta Calidad'
+                    radius='md'
+                    {...form.getInputProps('AfectaCalidad', {
+                      type: 'checkbox'
+                    })}
+                  />
+                </Group>
+                <Textarea
+                  label='Descripción'
+                  placeholder='Descripción'
+                  minRows={6}
+                  maxRows={6}
+                  {...form.getInputProps('Descripcion')}
+                />
+              </Stack>
             </Grid.Col>
             <Group>
-              <Button variant='contained' onClick={submitForm}> Mandar </Button>
-              <Button variant='outline' onClick={() => setOpen(false) } >
+              <Button variant='contained' onClick={submitForm}>
+                {' '}
+                Mandar{' '}
+              </Button>
+              <Button variant='outline' onClick={() => setOpen(false)}>
                 Random email
               </Button>
             </Group>
           </Grid>
-          </form>
+        </form>
       </Modal>
     </>
   )
   function getPuntaje(IdImpacto, IdPosibilidad) {
-    const impacto = ListaImpacto.find((item) => item.value === IdImpacto) || { puntaje: 0 }
-    const posibilidad = ListaPosibilidad.find((item) => item.value === IdPosibilidad) || { puntaje: 0 }
+    const impacto = ListaImpacto.find((item) => item.value === IdImpacto) || {
+      puntaje: 0
+    }
+    const posibilidad = ListaPosibilidad.find(
+      (item) => item.value === IdPosibilidad
+    ) || { puntaje: 0 }
     const nivelRiesgo = impacto.puntaje * posibilidad.puntaje
     return nivelRiesgo
   }
@@ -143,9 +220,9 @@ export default function Form({
 }
 function ItemList({ label, puntaje, ...others }) {
   return (
-    <Group { ... others }>
-      <div>{ label }</div>
-      <div>{ puntaje }</div>
+    <Group {...others}>
+      <div>{label}</div>
+      <div>{puntaje}</div>
     </Group>
   )
 }
@@ -161,7 +238,12 @@ function NivelRiesgoItem({ puntaje, texto, ...others }) {
   }
   if (puntaje !== 0) {
     return (
-      <Badge color={color} fullWidth variant="filled" { ...others }>{`${puntaje} - ${texto}`}</Badge>
+      <Badge
+        color={color}
+        fullWidth
+        variant='filled'
+        {...others}
+      >{`${puntaje} - ${texto}`}</Badge>
     )
   } else {
     return null
