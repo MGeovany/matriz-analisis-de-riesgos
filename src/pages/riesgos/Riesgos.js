24b75931aa9a7759be41b79350b9ef748fd82f4f
Geovany <marlongeo1999@gmail.com>
2022-08-23 09:49:12 -0600

    changed the name of form to Form and eliminar to Delete

--- src/pages/riesgos/Riesgos.js
+++ src/pages/riesgos/Riesgos.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import { Container, Button } from '@mantine/core'
-import Form from './form'
+import { Button } from '@mantine/core'
+import Form from './Form'
 import { Riesgo } from '../../API'
 import { TablaRiesgos } from './TablaRiesgo'
 import { Header } from '../../components/Header'
@@ -10,49 +10,74 @@ import './RiesgosStyles.css'
 export const Riesgos = () => {
   const [RiesgosList, setRiesgosList] = React.useState([])
   function onAdd(riesgo) {
-    Riesgo().save(riesgo).then(res => {
-      setRiesgosList([...RiesgosList, riesgo])
-    }).catch(error => { console.error(error.response) })
+    Riesgo()
+      .save(riesgo)
+      .then((res) => {
+        setRiesgosList([...RiesgosList, riesgo])
+      })
+      .catch((error) => {
+        console.error(error.response)
+      })
   }
 
   function onEdit(riesgo) {
     console.log(riesgo)
-    Riesgo().edit(riesgo).then(res => {
-      setRiesgosList([...RiesgosList.filter(r => r.Id !== riesgo.Id), riesgo])
-    }).catch(error => { console.error(error.response) })
+    Riesgo()
+      .edit(riesgo)
+      .then((res) => {
+        setRiesgosList([
+          ...RiesgosList.filter((r) => r.Id !== riesgo.Id),
+          riesgo
+        ])
+      })
+      .catch((error) => {
+        console.error(error.response)
+      })
   }
 
   function onDelete(riesgoId) {
-    Riesgo().delete(riesgoId).then(res => {
-      setRiesgosList([...RiesgosList.filter(r => r.Id !== riesgoId)])
-    }).catch(error => { console.error(error.response) })
+    Riesgo()
+      .delete(riesgoId)
+      .then((res) => {
+        setRiesgosList([...RiesgosList.filter((r) => r.Id !== riesgoId)])
+      })
+      .catch((error) => {
+        console.error(error.response)
+      })
   }
 
   React.useEffect(() => {
-    Riesgo().get().then(res => {
-      setRiesgosList(res)
-    }).catch(error => console.error(error))
+    Riesgo()
+      .get()
+      .then((res) => {
+        setRiesgosList(res)
+      })
+      .catch((error) => console.error(error))
   }, [])
 
   return (
     <>
-    <div className='side-container'>
-      <Header title='Riesgos' />
-      <Form trigger={<Button>Nuevo</Button>} handler={onAdd}/>
-      <div className='containerTabla'>
-        <TablaRiesgos onEdit={onEdit} onDelete={onDelete} elements={RiesgosList} />
-      </div>
-      <nav>
+      <div className='side-container'>
+        <Header title='Riesgos' />
+        <Form trigger={<Button>Nuevo</Button>} handler={onAdd} />
+        <div className='containerTabla'>
+          <TablaRiesgos
+            onEdit={onEdit}
+            onDelete={onDelete}
+            elements={RiesgosList}
+          />
+        </div>
+        <nav>
           <ul className='nav_links'>
             <li>
-              <Form trigger={<Button>Nuevo</Button>} handler={onAdd}/>
+              <Form trigger={<Button>Nuevo</Button>} handler={onAdd} />
             </li>
             <li>
               <Button variant='light'>Descargar PDF</Button>
             </li>
           </ul>
         </nav>
-    </div>
-  </>
+      </div>
+    </>
   )
 }
--- src/pages/riesgos/TablaRiesgo.js
+++ src/pages/riesgos/TablaRiesgo.js
@@ -3,17 +3,18 @@ import { Table } from '@mantine/core'
 import './RiesgosStyles.css'
 import './TablaRiesgo.css'
 import { IconEye, IconTrash } from '@tabler/icons'
-import Form from './form'
-import Eliminar from './eliminar'
+import Form from './Form'
+import Delete from './Delete'
 import { Opciones } from '../../API'
-const ojo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/OOjs_UI_icon_eye-progressive.svg/1024px-OOjs_UI_icon_eye-progressive.svg.png'
-const borrar = 'https://www.shareicon.net/data/512x512/2017/06/21/887258_delete_512x512.png'
 
 /* eslint-disable react/prop-types */
 export function TablaRiesgos({ elements = [], onEdit, onDelete }) {
   const [ListaNivelRiesgo, setListaNivelRiesgo] = React.useState([])
   React.useState(() => {
-    Opciones().NivelRiesgo().then(data => setListaNivelRiesgo(data)).catch(error => console.error(error))
+    Opciones()
+      .NivelRiesgo()
+      .then((data) => setListaNivelRiesgo(data))
+      .catch((error) => console.error(error))
   }, [])
 
   const rows = elements.map((element, index) => (
@@ -21,12 +22,22 @@ export function TablaRiesgos({ elements = [], onEdit, onDelete }) {
       <td>{element.Nombre}</td>
       <td>{`${element.PuntajeImpacto} - ${element.NivelImpacto}`}</td>
       <td>{`${element.PuntajePosibilidad} - ${element.NivelPosibilidad}`}</td>
-      <td>{`${element.NivelRiesgo} - ${getNivelRiesgo(element.NivelRiesgo)}`}</td>
+      <td>{`${element.NivelRiesgo} - ${getNivelRiesgo(
+        element.NivelRiesgo
+      )}`}</td>
       <td>
-      <Form trigger={<IconEye cursor='pointer' color='#228be6' />} handler={onEdit} riesgo={element}/>
+        <Form
+          trigger={<IconEye cursor='pointer' color='#228be6' />}
+          handler={onEdit}
+          riesgo={element}
+        />
       </td>
       <td>
-       <Eliminar trigger={<IconTrash cursor='pointer' color='#f05761' />} handler={onDelete} riesgo={element.Id} />
+        <Delete
+          trigger={<IconTrash cursor='pointer' color='#f05761' />}
+          handler={onDelete}
+          riesgo={element.Id}
+        />
       </td>
     </tr>
   ))
